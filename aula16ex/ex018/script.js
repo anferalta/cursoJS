b9decbee7a0f19ac686a60b4960c05fc84f3fe0b
anferalta <150276760+anferalta@users.noreply.github.com>
2024-05-21 20:48:40 +0100

    atualizei ex018

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -6,6 +6,7 @@ let valores = []
 function isNumero(n) {
     if (Number(n) >= 1 && Number(n) <= 100) {
         return true
+
     } else {
         return false
     }
@@ -14,6 +15,7 @@ function isNumero(n) {
 function inLista(n, l) {
     if (l.indexOf(Number(n)) != -1) {
         return true
+        
     } else {
         return false
     }
@@ -22,13 +24,16 @@ function inLista(n, l) {
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, valores)) {
         valores.push(Number(num.value))
+
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
-        res.innerHTML = ''       
+        res.innerHTML = '' 
+
     } else {
         alert('Valor inválido ou já encontrado na lista.')
     }
+
     num.value = ''
     num.focus()
 }
@@ -36,6 +41,7 @@ function adicionar() {
 function finalizar () {
     if (valores.length == 0) {
         alert('Adicione valores antes de finalizar!')
+
     } else {
         let tot = valores.length
         let maior = valores[0]
@@ -48,11 +54,13 @@ function finalizar () {
 
             if (valores[pos] > maior)
                 maior = valores[pos]
+
             if (valores[pos] < menor)
                 menor = valores[pos]
         }
 
         media = soma / tot
+
         res.innerHTML = ''
         res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
         res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
